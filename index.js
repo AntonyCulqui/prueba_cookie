12f9b134a756154420ba5b361ea8cd1a36426255
AntonyCulqui <112915051+AntonyCulqui@users.noreply.github.com>
2023-06-30 08:37:03 -0500

    test2

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
+import express from "express";
+import cookieParser from "cookie-parser";
 
-const app = require("./src/app")
-const port = process.env.PORT;
+const app =  express();
 
-app.listen(port, () => { 
-    console.log("Running on port", port);
-    console.log("Environment", process.env.NODE_ENV);
+app.use(cookieParser());
+
+app.get('/', (req,res)=>{
+    res.send('Hello World')
+})
+
+app.listen(3001, () => { 
+    console.log("Running on port", '3001');
+    console.log("Environment", "dev");
 });
